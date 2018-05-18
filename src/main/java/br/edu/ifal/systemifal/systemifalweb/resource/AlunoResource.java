f4dae234bc6967d96ecaf6abb349e5c07969e92d
ClarinhaSantos <anna.claara98@gmail.com>
2018-05-18 09:26:34 -0300

    Edição de AlunoResource

--- src/main/java/br/edu/ifal/systemifal/systemifalweb/resource/AlunoResource.java
+++ src/main/java/br/edu/ifal/systemifal/systemifalweb/resource/AlunoResource.java
@@ -3,6 +3,7 @@ package br.edu.ifal.systemifal.systemifalweb.resource;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -33,6 +34,11 @@ public class AlunoResource {
 	@RequestMapping(value="listar", method=RequestMethod.GET)
 	public List<Aluno> listar(){
 		return alunoRepository.findAll();
+	}
+	
+	@GetMapping("/deletar/{id}")
+	public void deletar(@PathVariable Integer id) {
+		alunoRepository.deleteById(id);
 	}
 	
 	
